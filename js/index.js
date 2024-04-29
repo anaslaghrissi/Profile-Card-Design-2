2df46d017e4ce9471d14de3e0f5e4c7425c2ad14
Anas Laghrissi <108026572+anaslaghrissi@users.noreply.github.com>
2024-04-29 13:50:50 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const userbox = document.getElementById("user-box");
+
+function openinfo() {
+  userbox.classList.remove("hide-details");
+}
+function closeinfo() {
+  userbox.classList.add("hide-details");
+}
